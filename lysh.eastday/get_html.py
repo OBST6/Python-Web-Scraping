c505bf9f11baedaddb510548d89328b968055440
OBST6 <just4PYthon>
2017-09-25 17:27:25 +0800

    Shanghai 景区实时信息 initial_version

--- lysh.eastday/get_html.py
+++ lysh.eastday/get_html.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+
 
 url = 'http://lysh.eastday.com/#xxcx'
 html = requests.get(url)
@@ -16,17 +18,34 @@ target.close()
 
 
 
-
-url = 'http://lyjs.eastday.com/lyj/WebApiService/api/GetSpots?t=%s' % 20170922165700
+#url = 'http://lyjs.eastday.com/lyj/WebApiService/api/GetSpots?t=%s' % 20170922165700
+url = 'http://lyjs.eastday.com/lyj/WebApiService/api/GetSpots'
 html = requests.get(url)
 # html.encoding = 'gb2312'
 # print (html.text)
 print(type(html.text))
 html_str = html.text
-print(html_str)
+list1 = html_str.split('],"', 1)
+list2 = list1[0].split(':[')
+list3 = list2[1][1:-1].split(',')
+list4 = list3[2].split(':')
+cur_num = list4[1][1:-1] + '\n'
+print(list1[0])
+print(list2[1])
+print(list3[2])
+print(list4)
+print(type(cur_num))
+print(cur_num)
 
 
 target = open('get2.html', 'w', encoding = 'utf-8')
 target.write(html_str)
 target.close()
+
+
+target = open('cur_num.csv', 'a')
+target.write(cur_num)
+target.close()
+
+
 
